7f1b9a66d8e5c676b01462598624f64854c9ee12
David. J <codewithjalloh@gmail.com>
2022-06-01 21:44:09 +0100

    step-5

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
+
+
 # Create your views here.
 
 
@@ -49,3 +54,30 @@ def counter(request):
     text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'username already used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+
+        else:
+            messages.info(request, 'Password not the same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
